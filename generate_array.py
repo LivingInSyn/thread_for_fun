93918509a48fc1a689e1c6d3a0b80dfac69de7cf
Jeremy Mill <jeremymill@gmail.com>
2015-12-16 20:19:24 -0500

    initial commit

---
+++ generate_array.py
@@ -0,0 +1,9 @@
+from random import randint
+
+a = []
+for x in range(0,9999):	
+	a.append(randint(1,10))
+	
+print(a)
+	
+	
---
+++ twoThreads/two_threads.h
@@ -0,0 +1,8 @@
+/* two_threads.h */
+
+typedef struct partial_sum_struct
+{
+	int start;
+	int stop;
+	int* array;
+} PARTIAL_SUM_STRUCT;
